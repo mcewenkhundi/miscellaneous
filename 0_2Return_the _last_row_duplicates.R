78e378b74e62d13cd7d1512cd6af9a315be24e8f
mcewenkhundi <mcewenkhundi@gmail.com>
2018-10-04 18:04:00 +0200

    Return the unique records and the first duplicated row/the last duplicated

---
+++ 0_2Return_the _last_row_duplicates.R
@@ -0,0 +1,19 @@
+#Return the unique records and the last duplicated row
+
+library(dplyr)
+df <- data.frame(id = c(1,2,3,1,3,4,3), ch = c(1:7))
+
+df
+
+#This returns the unique records and the first duplicated row 
+
+df %>%
+  group_by(id) %>%
+  filter(row_number() == 1)
+ 
+#This returns the unique records and the last duplicated row
+df %>%
+  group_by(id) %>%
+  filter(row_number() == max(row_number()))
+         
+         
