3a70d16c1ead803c0deb53b4e3329be0b10f2cc6
mcewenkhundi <mcewenkhundi@gmail.com>
2018-10-09 08:54:22 +0200

    solution to rowwise percents

--- 03_row_wise_tables.R
+++ 03_row_wise_tables.R
@@ -2,8 +2,16 @@
 #How to calculate row-wise percentages in a using the arsenal package
 #The code below produces col-wise percentages, but i would like to produce row-wise percentages. 
 
-library(arsenal )
+library(arsenal)
 
-tab1 <- tableby(arm ~ sex + race, data=mockstudy)
+ df <-  data.frame(arm = sample(c("Int","Contr"),size=10, replace = T), 
+                  sex = sample(c("Male", "Female"),size=10, replace = T),
+                 agegp = sample(c("<15","15-49",">50"),size=10, replace = T))
+
+tab1 <- tableby(arm ~ sex + agegp, data=df)
 
 summary(tab1, text=TRUE)
+
+#Solution
+summary(tableby(arm ~ sex + agegp, data = df, cat.stats = "countrowpct"), text = TRUE)
+
