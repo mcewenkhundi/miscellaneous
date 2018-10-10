770384ee326b780d4f61ec36d505eb1ab556fe73
mcewenkhundi <mcewenkhundi@gmail.com>
2018-10-10 07:24:08 +0200

    categorical to dummy variables

---
+++ 04_Categorical_dummies.R
@@ -0,0 +1,8 @@
+library(fastDummies)
+
+crime <- data.frame(city = c("SF", "SF", "NYC"),
+                    year = c(1990, 2000, 1990),
+                    crime = 1:3)
+crime
+
+dummy_cols(crime)
