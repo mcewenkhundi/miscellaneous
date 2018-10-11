0c685d9b37978398e4475a6ead23c9f3717c7443
mcewenkhundi <mcewenkhundi@gmail.com>
2018-10-11 07:34:10 +0200

    column splitting

---
+++ 05_col_split_to_mult_cols.R
@@ -0,0 +1,15 @@
+
+#https://stackoverflow.com/questions/4350440/split-data-frame-string-column-into-multiple-columns
+
+library(dplyr)
+library(tidyr)
+
+before <- data.frame(
+  attr = c(1, 30 ,4 ,6 ), 
+  type = c('foo_and_bar', 'foo_and_bar_2')
+)
+
+before %>%
+  separate(type, c("foo", "bar"), "_and_")
+
+before
